59cfd8b682d9180441ad5bec38c9408e03b875ff
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-03-22 21:24:24 -0300

    added google charts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+
+declare var google: any;
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,274 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  private data: any;
+  private histogramData: any;
+  private comboData: any;
+  private areaData: any;
+  private lineData: any;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(): void {
+
+    this.dataService.getData().subscribe(
+      data => {
+        this.data = data;
+        this.init();
+      }
+    )
+
+    this.dataService.getHistogramData().subscribe(
+      histogramData => {
+        this.histogramData = histogramData;
+        this.init();
+      }
+    )
+
+    this.dataService.getComboData().subscribe(
+      comboData => {
+        this.comboData = comboData;
+        this.init();
+      }
+    )
+
+    this.dataService.getAreaData().subscribe(
+      areaData => {
+        this.areaData = areaData;
+        this.init();
+      }
+    )
+
+    this.dataService.getLineData().subscribe(
+      lineData => {
+        this.lineData = lineData;
+        this.init();
+      }
+    )
+
+  }
+
+  init(): void {
+    if (typeof (google) !== 'undefined') {
+      google.charts.load('current', { 'packages': ['corechart'] });
+      setTimeout(() => {
+        google.charts.setOnLoadCallback(this.showGraphics());
+      }, 1000)
+    }
+  }
+
+  showGraphics(): void {
+    this.showPieChart();
+    this.show3dPieChart();
+    this.showBarChart();
+    this.showLineChart();
+    this.showColumnChart();
+    this.showDonutChart();
+    this.showScatterChart();
+    this.showHistogram();
+    this.showComboChart();
+    this.showAreaChart();
+    this.showALineChart();
+  }
+
+  showPieChart(): void {
+    const el = document.getElementById('pie_chart');
+    const chart = new google.visualization.PieChart(el);
+
+    chart.draw(this.getDataTable(), this.getOptions());
+  }
+
+  show3dPieChart(): void {
+    const el = document.getElementById('3d_pie_chart');
+    const chart = new google.visualization.PieChart(el);
+    const options = this.getOptions();
+
+    options['is3D'] = true;
+    chart.draw(this.getDataTable(), options);
+  }
+
+  showDonutChart(): void {
+    const el = document.getElementById('donut_chart');
+    const chart = new google.visualization.PieChart(el);
+    const options = this.getOptions();
+
+    options['pieHole'] = 0.4;
+    chart.draw(this.getDataTable(), options);
+  }
+
+  showBarChart(): void {
+    const el = document.getElementById('bar_chart');
+    const chart = new google.visualization.BarChart(el);
+
+    chart.draw(this.getDataTable(), this.getOptions());
+  }
+
+  showLineChart(): void {
+    const el = document.getElementById('line_chart');
+    const chart = new google.visualization.LineChart(el);
+
+    chart.draw(this.getDataTable(), this.getOptions());
+  }
+
+  showColumnChart(): void {
+    const el = document.getElementById('column_chart');
+    const chart = new google.visualization.ColumnChart(el);
+
+    chart.draw(this.getDataTable(), this.getOptions());
+  }
+
+  showScatterChart(): void {
+    const el = document.getElementById('scatter_chart');
+    const chart = new google.visualization.ScatterChart(el);
+
+    chart.draw(this.getDataTable(), this.getScatterOptions());
+  }
+
+  showHistogram(): void {
+    const el = document.getElementById('histogram');
+    const chart = new google.visualization.Histogram(el);
+
+    chart.draw(this.getHistogramDataTable(), this.getHistogramOptions());
+  }
+
+  showComboChart(): void {
+    const el = document.getElementById('combo_chart');
+    const chart = new google.visualization.ComboChart(el);
+
+    chart.draw(this.getComboChartDataTable(), this.getComboChartOptions());
+  }
+
+  showAreaChart(): void {
+    const el = document.getElementById('area_chart');
+    const chart = new google.visualization.SteppedAreaChart(el);
+
+    chart.draw(this.getAreaChartDataTable(), this.getAreaChartOptions());
+  }
+
+  showALineChart(): void {
+    const el = document.getElementById('lineData_chart');
+    const chart = new google.visualization.LineChart(el);
+
+    chart.draw(this.getLineChartDataTable(), this.getLineChartOptions());
+  }
+
+  getDataTable(): any {
+    const data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Mês');
+    data.addColumn('number', 'Quantidade');
+    data.addRows(this.data);
+
+    return data;
+  }
+
+  getOptions(): any {
+    return {
+      'title': 'Quantidade de Cadastros Primeiro Semestre',
+      'width': 400,
+      'height': 300,
+    };
+  }
+
+  getHistogramDataTable(): any {
+    const data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Dinosaur');
+    data.addColumn('number', 'Length');
+    data.addRows(this.histogramData);
+
+    return data;
+  }
+
+  getHistogramOptions(): any {
+    return {
+      'title': 'Lengths of dinosaurs, in meters',
+      'width': 400,
+      'height': 300,
+    };
+  }
+
+  getComboChartDataTable(): any {
+    const data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Month');
+    data.addColumn('number', 'Bolivia');
+    data.addColumn('number', 'Ecuador');
+    data.addColumn('number', 'Madagascar');
+    data.addColumn('number', 'Papua New Guinea');
+    data.addColumn('number', 'Rwanda');
+    data.addColumn('number', 'Average');
+    data.addRows(this.comboData);
+
+    return data;
+  }
+
+  getComboChartOptions(): any {
+    return {
+      'title': 'Monthly Coffee Production by Country',
+      'vAxis': { title: 'Cups' },
+      'hAxis': { title: 'Month' },
+      'seriesType': 'bars',
+      'series': { 5: { type: 'line' } },
+      'width': 800,
+      'height': 600,
+    };
+  }
+
+  getAreaChartDataTable(): any {
+    const data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Director (Year)');
+    data.addColumn('number', 'Rotten Tomatoes');
+    data.addColumn('number', 'IMDB');
+    data.addRows(this.areaData);
+
+    return data;
+  }
+
+  getAreaChartOptions(): any {
+    return {
+      'title': 'The decline of \'The 39 Steps\'',
+      'vAxis': { title: 'Accumulated Rating' },
+      'isStacked': true,
+      'width': 800,
+      'height': 600,
+    };
+  }
+
+  getLineChartDataTable(): any {
+    const data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Year');
+    data.addColumn('number', 'Sales');
+    data.addColumn('number', 'Expenses');
+    data.addRows(this.lineData);
+
+    return data;
+  }
+
+  getLineChartOptions(): any {
+    return {
+      'title': 'Company Performance',
+      'curveType': 'function',
+      'width': 800,
+      'height': 600,
+    };
+  }
+
+  getScatterOptions(): any {
+    return {
+      'title': 'Age vs. Weight comparison',
+      'hAxis': { title: 'Mes', minValue: 0, maxValue: 15 },
+      'vAxis': { title: 'Quantidade', minValue: 0, maxValue: 15 },
+      'legend': 'none',
+      'width': 400,
+      'height': 300,
+    };
   }
 
 }
--- src/app/dashboard/data.service.ts
+++ src/app/dashboard/data.service.ts
@@ -1,9 +1,114 @@
 import { Injectable } from '@angular/core';
+import { observable, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  readonly data = [
+    ['Janeiro', 33],
+    ['Fevereiro', 68],
+    ['Março', 49],
+    ['Abril', 15],
+    ['Maio', 80],
+    ['Junho', 27],
+  ];
+
+  readonly histogramData = [
+    ['Acrocanthosaurus (top-spined lizard)', 12.2],
+    ['Albertosaurus (Alberta lizard)', 9.1],
+    ['Allosaurus (other lizard)', 12.2],
+    ['Apatosaurus (deceptive lizard)', 22.9],
+    ['Archaeopteryx (ancient wing)', 0.9],
+    ['Argentinosaurus (Argentina lizard)', 36.6],
+    ['Baryonyx (heavy claws)', 9.1],
+    ['Brachiosaurus (arm lizard)', 30.5],
+    ['Ceratosaurus (horned lizard)', 6.1],
+    ['Coelophysis (hollow form)', 2.7],
+    ['Compsognathus (elegant jaw)', 0.9],
+    ['Deinonychus (terrible claw)', 2.7],
+    ['Diplodocus (double beam)', 27.1],
+    ['Dromicelomimus (emu mimic)', 3.4],
+    ['Gallimimus (fowl mimic)', 5.5],
+    ['Mamenchisaurus (Mamenchi lizard)', 21.0],
+    ['Megalosaurus (big lizard)', 7.9],
+    ['Microvenator (small hunter)', 1.2],
+    ['Ornithomimus (bird mimic)', 4.6],
+    ['Oviraptor (egg robber)', 1.5],
+    ['Plateosaurus (flat lizard)', 7.9],
+    ['Sauronithoides (narrow-clawed lizard)', 2.0],
+    ['Seismosaurus (tremor lizard)', 45.7],
+    ['Spinosaurus (spiny lizard)', 12.2],
+    ['Supersaurus (super lizard)', 30.5],
+    ['Tyrannosaurus (tyrant lizard)', 15.2],
+    ['Ultrasaurus (ultra lizard)', 30.5],
+    ['Velociraptor (swift robber)', 1.8]
+  ];
+
+  readonly comboData = [
+    ['2004/05', 165, 938, 522, 998, 450, 614.6],
+    ['2005/06', 135, 1120, 599, 1268, 288, 682],
+    ['2006/07', 157, 1167, 587, 807, 397, 623],
+    ['2007/08', 139, 1110, 615, 968, 215, 609.4],
+    ['2008/09', 136, 691, 629, 1026, 366, 569.6]
+  ];
+
+  readonly areaData = [
+    ['Alfred Hitchcock (1935)', 8.4, 7.9],
+    ['Ralph Thomas (1959)', 6.9, 6.5],
+    ['Don Sharp (1978)', 6.5, 6.4],
+    ['James Hawes (2008)', 4.4, 6.2]
+  ];
+
+  readonly lineData = [
+    ['2004', 1000, 400],
+    ['2005', 1170, 460],
+    ['2006', 660, 1120],
+    ['2007', 1030, 540],
+    ['2008', 845, 300],
+    ['2009', 1200, 640],
+    ['2010', 900, 230],
+    ['2011', 450, 670],
+    ['2012', 100, 440],
+    ['2013', 1320, 350],
+    ['2014', 1500, 210],
+  ];
+
   constructor() { }
+
+  getData(): Observable<any> {
+    return new Observable(observable => {
+      observable.next(this.data);
+      observable.complete();
+    })
+  }
+
+  getHistogramData(): Observable<any> {
+    return new Observable(observable => {
+      observable.next(this.histogramData);
+      observable.complete();
+    })
+  }
+
+  getComboData(): Observable<any> {
+    return new Observable(observable => {
+      observable.next(this.comboData);
+      observable.complete();
+    })
+  }
+
+  getAreaData(): Observable<any> {
+    return new Observable(observable => {
+      observable.next(this.areaData);
+      observable.complete();
+    })
+  }
+
+  getLineData(): Observable<any> {
+    return new Observable(observable => {
+      observable.next(this.lineData);
+      observable.complete();
+    })
+  }
 }
